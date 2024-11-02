c63705072039685966bd284f321ef5a76c245907
Sherajus-Salehin <66803588+Sherajus-Salehin@users.noreply.github.com>
2024-11-02 22:45:45 +0600

    Classes.java needs fix, testing in Main.java.

--- FrameBuilding/src/FrameBuild/Classes.java
+++ FrameBuilding/src/FrameBuild/Classes.java
@@ -3,11 +3,22 @@ package FrameBuild;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Objects;
 
 public class Classes {
-    public static List<Class<?>> scan(File dir, String packageName) throws ClassNotFoundException{
-        //later
-        return List.of();
+    public static List<Class<?>> scan(File dir, String packageName) throws ClassNotFoundException {
+        List<Class<?>> classList = new ArrayList<>();
+        if (dir.listFiles() == null) {
+            System.out.println("Directory Error");
+        }
+        for(File file : Objects.requireNonNull(dir.listFiles())) {
+            if(file.isFile() && file.getName().endsWith(".class")) {
+                String className = packageName + "." + file.getName().replace(".class", "");
+
+                classList.add(Class.forName(className)); // forName returns instance of this named class
+            }
+            else classList.addAll(scan(file, packageName + "." + file.getName()));
+        }
+        return classList;
     }
 }
