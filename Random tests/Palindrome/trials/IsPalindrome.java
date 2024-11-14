0f32e764856dbcd3e8a03191a34963772a3e325b
Sherajus-Salehin <66803588+Sherajus-Salehin@users.noreply.github.com>
2024-11-15 02:38:22 +0600

    Code for finding all palindrome substrings in a given string. works

--- Random tests/Palindrome/trials/IsPalindrome.java
+++ Random tests/Palindrome/trials/IsPalindrome.java
@@ -1,16 +1,38 @@
 package Palindrome.trials;
-
 public class IsPalindrome {
-    static void IsEPalindrome(char[] e){
-        for(char item:e){
-            System.out.print(item);
+    static boolean p=false;
+    static void check(char[] c){
+        p=false;
+        int i=0;
+        int j=c.length;
+        if(j%2==0) IsEPalindrome(c,i,(j-1));
+        else IsOPalindrome(c,i,(j-1));
+        if(p){
+            for (char item:c){
+                System.out.print(item);
+            }System.out.println();
         }
-        System.out.println();
     }
-    static void IsOPalindrome(char[] o){
-        for(char item:o){
-            System.out.print(item);
+
+    static void IsEPalindrome(char[] e, int i, int j) {
+        if(e[i]==e[j]){
+            if(i==(j+1)){
+                p=true;
+                return;
+            }
+            i++;j--;
+            IsEPalindrome(e,i,j);
         }
-        System.out.println();
+    }
+
+    static void IsOPalindrome(char[] o, int i, int j) {
+        if(o[i]==o[j]){
+            if(i==j){
+                p=true;
+                return;
+            }
+            i++;j--;
+            IsOPalindrome(o,i,j);
+        }
     }
 }
--- Random tests/Palindrome/trials/recursive.java
+++ Random tests/Palindrome/trials/recursive.java
@@ -8,20 +8,11 @@ public class recursive {
         char[]words=word.toCharArray();
         int l=words.length;
         for (int i=2;i<=l;i++){
-            if(i%2==0){
                 int j=0;
                 while (j<=l-i){
-                    IsPalindrome.IsEPalindrome(Arrays.copyOfRange(words,j,j+i));
-                    j+=i;
-                }
-
-            }else {
-                int j=0;
-                while (j<=l-i){
-                    IsPalindrome.IsEPalindrome(Arrays.copyOfRange(words,j,j+i));
-                    j+=i;
+                    IsPalindrome.check(Arrays.copyOfRange(words,j,j+i));
+                    j++;
                 }
             }
-        }
     }
 }
