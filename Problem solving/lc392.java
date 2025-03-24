be3f673d34dc04ed5cfbcadfc9162c0c39047b5a
Sherajus-Salehin <66803588+Sherajus-Salehin@users.noreply.github.com>
2025-03-25 02:44:17 +0600

    some leetcode problems

--- Problem solving/lc392.java
+++ Problem solving/lc392.java
@@ -2,25 +2,27 @@ public class lc392 {
     char [] sc;
     char [] tc;
     int cr=0;
-        public boolean isSubsequence(String s, String t) {
-            String T=t;
-            int l=s.length();
-            sc=s.toCharArray();
-            tc=t.toCharArray();
-            for(int i=0;i<l;i++){
-             cr=charFinder(sc[i],cr);
-             if(cr==-1){
-                 return false;
-             }
+    public boolean isSubsequence(String s, String t) {
+        int l=s.length();
+        if(l==t.length())
+            return s.equals(t);
+        sc=s.toCharArray();
+        tc=t.toCharArray();
+        for(int i=0;i<l;i++){
+            cr=charFinder(sc[i],cr);
+            if(cr==-1){
+                return false;
             }
-            return cr!=-1;
+            cr++;
         }
-        int charFinder(char c,int x){
-            for(int j=x;j<tc.length;j++){
-                if(tc[j]==c) {
-                    return j;
-                }
+        return cr!=-1;
+    }
+    int charFinder(char c,int x){
+        for(int j=x;j<tc.length;j++){
+            if(tc[j]==c) {
+                return j;
             }
-            return -1;
         }
+        return -1;
+    }
 }
